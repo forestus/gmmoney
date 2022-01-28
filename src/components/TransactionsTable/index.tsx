a6f76a5c4d3afbf64b1b75b23978fe02877a74e5
Guilherme Pereira Maciel <guilhermepm164@gmail.com>
2022-01-28 00:06:36 -0300

    chore: add list transactions with formated date and number field

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,37 +1,55 @@
-import { useEffect } from "react";
-import { api } from "../../services/api";
-import { Container } from "./styles";
+import { useEffect, useState } from 'react';
+import { api } from '../../services/api';
+import { Container } from './styles';
 
 export function TransactionsTable() {
-    useEffect(()=>{
-        api.get("transactions").then(data=>console.log(data))
-    },[])
-    return (
+  type Transaction = {
+    id: number;
+    title: string;
+    ammount: number;
+    type: 'deposit' | 'withdraw';
+    category: string;
+    createdAt: string;
+  };
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  useEffect(() => {
+    api
+      .get('transactions')
+      .then((response) => setTransactions(response.data.transactions));
+  }, []);
+  return (
     <Container>
-        <table>
-            <thead>
-                <tr>
-                    <th>Título</th>
-                    <th>Valor</th>
-                    <th>Categoria</th>
-                    <th>Data</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Desenvolvimento de website</td>
-                    <td className="deposit">R$12.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/02/2021</td>
-                </tr>
-                <tr>
-                    <td>Aluguel</td>
-                    <td className="withdraw">- R$1.000</td>
-                    <td>Desenvolvimento</td>
-                    <td>20/02/2021</td>
-                </tr>
-            </tbody>
-        </table>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.length &&
+            transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL',
+                  }).format(transaction.ammount)}
+                </td>
+                <td>{transaction.category}</td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR', {
+                    dateStyle: 'full',
+                    timeStyle: 'short',
+                  }).format(new Date(transaction.createdAt))}
+                </td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
     </Container>
-    );
+  );
 }
